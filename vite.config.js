7abfd51465230321270a6227529fc5adeaa8363c
AnishKumar-12345 <anishkumarbehera32@gmail.com>
2025-06-18 11:09:29 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './', // This ensures relative paths
-  build: {
-    rollupOptions: {
-      external: ['vue', 'vuetify', 'tsparticles'], // Add other dependencies if needed
-      output: {
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
 })
